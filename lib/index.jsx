2e53f3f1f8cce8c83634d880cc85716745d64bb6
Shanon Levenherz <shanonvl@gmail.com>
2018-05-01 16:11:44 -0400

    Add onDraw(ImageData)

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import assign from 'object-assign'
 
 class DrawableCanvas extends React.Component {
 
-  componentDidMount(){
-    const canvas = ReactDOM.findDOMNode(this);
+  componentDidMount() {
+    const canvas = ReactDOM.findDOMNode(this)
 
-    canvas.style.width = '100%';
-    canvas.style.height = '100%';
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+    canvas.style.width = '100%'
+    canvas.style.height = '100%'
+    canvas.width = canvas.offsetWidth
+    canvas.height = canvas.offsetHeight
 
-    const context = canvas.getContext('2d');
+    const context = canvas.getContext('2d')
 
     this.setState({
       canvas,
       context
-    });
+    })
   }
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.clear){
-      this.resetCanvas();
+    if (nextProps.clear) {
+      this.resetCanvas()
     }
   }
+
   static getDefaultStyle() {
     return {
       brushColor: '#FFFF00',
@@ -35,104 +36,109 @@ class DrawableCanvas extends React.Component {
         backgroundColor: '#00FFDC'
       },
       clear: false
-    };
+    }
   }
 
-  handleOnTouchStart (e) {
-    const rect = this.state.canvas.getBoundingClientRect();
-    this.state.context.beginPath();
+  handleOnTouchStart(e) {
+    const rect = this.state.canvas.getBoundingClientRect()
+    this.state.context.beginPath()
     this.setState({
       lastX: e.targetTouches[0].pageX - rect.left,
       lastY: e.targetTouches[0].pageY - rect.top,
       drawing: true
-    });
+    })
   }
 
-  handleOnMouseDown(e){
-    const rect = this.state.canvas.getBoundingClientRect();
-    this.state.context.beginPath();
+  handleOnMouseDown(e) {
+    const rect = this.state.canvas.getBoundingClientRect()
+    this.state.context.beginPath()
 
     this.setState({
       lastX: e.clientX - rect.left,
       lastY: e.clientY - rect.top,
       drawing: true
-    });
+    })
   }
 
-  handleOnTouchMove (e) {
+  handleOnTouchMove(e) {
     if (this.state.drawing) {
-      const rect = this.state.canvas.getBoundingClientRect();
-      const lastX = this.state.lastX;
-      const lastY = this.state.lastY;
-      let currentX = e.targetTouches[0].pageX - rect.left;
-      let currentY = e.targetTouches[0].pageY - rect.top;
-      this.draw(lastX, lastY, currentX, currentY);
+      const rect = this.state.canvas.getBoundingClientRect()
+      const lastX = this.state.lastX
+      const lastY = this.state.lastY
+      let currentX = e.targetTouches[0].pageX - rect.left
+      let currentY = e.targetTouches[0].pageY - rect.top
+      this.draw(lastX, lastY, currentX, currentY)
       this.setState({
         lastX: currentX,
         lastY: currentY
-      });
+      })
     }
   }
 
-  handleOnMouseMove(e){
-    if(this.state.drawing){
-      const rect = this.state.canvas.getBoundingClientRect();
-      const lastX = this.state.lastX;
-      const lastY = this.state.lastY;
-      let currentX = e.clientX - rect.left;
-      let currentY = e.clientY - rect.top;
+  handleOnMouseMove(e) {
+    if (this.state.drawing) {
+      const rect = this.state.canvas.getBoundingClientRect()
+      const lastX = this.state.lastX
+      const lastY = this.state.lastY
+      let currentX = e.clientX - rect.left
+      let currentY = e.clientY - rect.top
 
-      this.draw(lastX, lastY, currentX, currentY);
+      this.draw(lastX, lastY, currentX, currentY)
       this.setState({
         lastX: currentX,
         lastY: currentY
-      });
+      })
     }
   }
 
-  handleonMouseUp() {
+  handleOnMouseUp() {
     this.setState({
       drawing: false
-    });
+    })
+    const {onDraw} = this.props
+    if (onDraw) {
+      const {context} = this.state
+      onDraw(context.getImageData(0, 0, context.canvas.width, context.canvas.height), context)
+    }
   }
 
   draw(lX, lY, cX, cY) {
-    const newContext = this.state.context;
-    newContext.strokeStyle = this.props.brushColor;
-    newContext.lineWidth = this.props.lineWidth;
+    const newContext = this.state.context
+    newContext.strokeStyle = this.props.brushColor
+    newContext.lineWidth = this.props.lineWidth
     this.setState({
       context: newContext
-    });
-    this.state.context.moveTo(lX, lY);
-    this.state.context.lineTo(cX, cY);
-    this.state.context.stroke();
+    })
+    this.state.context.moveTo(lX, lY)
+    this.state.context.lineTo(cX, cY)
+    this.state.context.stroke()
   }
 
-  resetCanvas(){
-    const width = this.state.context.canvas.width;
-    const height = this.state.context.canvas.height;
-    this.state.context.clearRect(0, 0, width, height);
+  resetCanvas() {
+    const width = this.state.context.canvas.width
+    const height = this.state.context.canvas.height
+    this.state.context.clearRect(0, 0, width, height)
   }
 
-  canvasStyle(){
-    const defaults = DrawableCanvas.getDefaultStyle();
-    const custom = this.props.canvasStyle;
+  canvasStyle() {
+    const defaults = DrawableCanvas.getDefaultStyle()
+    const custom = this.props.canvasStyle
 
-    return assign({}, defaults, custom);
+    return assign({}, defaults, custom)
   }
 
   render() {
     return (
-      <canvas style = {this.canvasStyle()}
-        onMouseDown = {this.handleOnMouseDown.bind(this)}
-        onTouchStart = {this.handleOnTouchStart.bind(this)}
-        onMouseMove = {this.handleOnMouseMove.bind(this)}
-        onTouchMove = {this.handleOnTouchMove.bind(this)}
-        onMouseUp = {this.handleonMouseUp.bind(this)}
-        onTouchEnd = {this.handleonMouseUp.bind(this)}
+      <canvas style={this.canvasStyle()}
+              onMouseDown={this.handleOnMouseDown.bind(this)}
+              onTouchStart={this.handleOnTouchStart.bind(this)}
+              onMouseMove={this.handleOnMouseMove.bind(this)}
+              onTouchMove={this.handleOnTouchMove.bind(this)}
+              onMouseUp={this.handleOnMouseUp.bind(this)}
+              onTouchEnd={this.handleOnMouseUp.bind(this)}
       >
       </canvas>
-    );
+    )
   }
 
 }
@@ -144,7 +150,8 @@ DrawableCanvas.propTypes = {
   canvasStyle: PropTypes.shape({
     backgroundColor: PropTypes.string
   }),
-  clear: PropTypes.bool
-};
+  clear: PropTypes.bool,
+  onDraw: PropTypes.func
+}
 
-export default DrawableCanvas;
+export default DrawableCanvas
